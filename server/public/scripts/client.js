d0826e2740851bd7df8ec3af7333b4db6e6e9ffb
Adam Biessener <abiessener@gmail.com>
2017-08-19 11:40:31 -0500

    GET route functionality

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -9,28 +9,41 @@ app.controller('EmployeeController', ['$http', function($http){
     self.pageTitle = 'Weekend Challenge 4: Full-Stack Employee Salary App';
 
     self.calcResult = 3456;
-    
-    // test GET route
-    $http({
-        method: 'GET',
-        url: '/list'
-    }).then(function(response){
-        console.log('GET response:', response);    
-    });
+    self.employeeList = [];
 
-    // test POST route
-    $http({
-        method: 'POST',
-        url: '/list'
-    }).then(function(response){
-        console.log('POST response:', response);
-    });
+    self.getList = function(){
+        $http({
+            method: 'GET',
+            url: '/list'
+        }).then(function(response){
+            console.log('GET response:', response);
+            self.employeeList = response.data;
+        });    
+    }
 
-    // test PUT route
-    $http({
-        method: 'PUT',
-        url: '/list'
-    }).then(function(response){
-        console.log('PUT response:', response);
-    });
+    self.getList();
+
+    // // test GET route
+    // $http({
+    //     method: 'GET',
+    //     url: '/list'
+    // }).then(function(response){
+    //     console.log('GET response:', response);    
+    // });
+
+    // // test POST route
+    // $http({
+    //     method: 'POST',
+    //     url: '/list'
+    // }).then(function(response){
+    //     console.log('POST response:', response);
+    // });
+
+    // // test PUT route
+    // $http({
+    //     method: 'PUT',
+    //     url: '/list'
+    // }).then(function(response){
+    //     console.log('PUT response:', response);
+    // });
 }]);
--- server/routes/list.js
+++ server/routes/list.js
@@ -3,9 +3,24 @@ var router = express.Router();
 var pool = require('../modules/pool');
 
 router.get('/', function(req,res){
-console.log('/list GET hit');
-res.sendStatus(200);
-});
+    console.log('/list GET hit');
+    pool.connect(function(err,db,done){
+        if(err){
+            console.log('error connecting to db:', err);
+            res.sendStatus(500);
+        } else {
+            db.query('SELECT * FROM employees', function(err,result){
+                done();
+                if(err){
+                    console.log('error making query');
+                    res.sendStatus(500);
+                } else {
+                    res.send(result.rows);
+                }
+            }); // end query
+        }
+    }); // end connect
+}); // end GET
 
 router.post('/', function(req,res){
     console.log('/list POST hit');
